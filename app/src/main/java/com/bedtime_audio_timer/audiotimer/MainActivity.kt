0251bffd62ac9c41b2866865bc86c11df2cf40d1
Willa Danielle Rawlinson <willa.rawlinson@gmail.com>
2018-07-24 12:55:30 -0700

    creating a basic timer which performs an action at evenly spaced intervals

--- app/src/main/java/com/bedtime_audio_timer/audiotimer/MainActivity.kt
+++ app/src/main/java/com/bedtime_audio_timer/audiotimer/MainActivity.kt
@@ -2,12 +2,17 @@ package com.bedtime_audio_timer.audiotimer
 
 import android.content.Context
 import android.content.Context.AUDIO_SERVICE
+import android.icu.util.UniversalTimeScale.toLong
 import android.media.AudioManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import com.bedtime_audio_timer.audiotimer.R
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+import java.util.concurrent.TimeUnit
+import kotlin.concurrent.schedule
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,13 +28,36 @@ class MainActivity : AppCompatActivity() {
             oldVol = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC)
             targetVol = oldVol/2
             audioManager.setVolume(targetVol)
+            timerWithIntervals(5 , 30, audioManager)
         }
 
+    }
+
+    private fun timerHelper(numMillis: Long){
+        Timer("interval timer", false).schedule(numMillis) {
+
+        }
     }
 
     private fun AudioManager.setVolume(volumeIndex: Int){
         this.setStreamVolume(
                 AudioManager.STREAM_MUSIC, volumeIndex, AudioManager.FLAG_SHOW_UI
         )
+    }
+
+    private fun timerWithIntervals(numMinutes: Int, numIntervals: Int, am: AudioManager){
+        val intervalLength = findEqualIntervalsInMilliseconds(numMinutes, numIntervals)
+        for (interval in 1..numIntervals){
+            val myToast = Toast.makeText(this, "Interval passed", Toast.LENGTH_SHORT) //delete when audio behavior has
+                                                                                                    // been defined for this function
+            Timer("interval timer", false).schedule(intervalLength*interval) {
+                myToast.show() //replace this line with desired audio behavior
+            }
+        }
+    }
+
+    private fun findEqualIntervalsInMilliseconds(numMinutes: Int, numIntervals: Int): Long {
+        val numMilliseconds = TimeUnit.MINUTES.toMillis(numMinutes.toLong())
+        return numMilliseconds/numIntervals.toLong()
     }
 }
