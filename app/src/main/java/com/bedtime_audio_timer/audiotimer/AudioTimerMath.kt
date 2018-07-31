e51a5f489fd4994a61c2ac04f9809e98301347ec
Willa Danielle Rawlinson <willa.rawlinson@gmail.com>
2018-07-31 16:05:02 -0700

    moving findEqualIntervalsInMilliseconds function from MainActivity.kt to AudioTimerMath.kt

--- app/src/main/java/com/bedtime_audio_timer/audiotimer/AudioTimerMath.kt
+++ app/src/main/java/com/bedtime_audio_timer/audiotimer/AudioTimerMath.kt
@@ -1,6 +1,7 @@
 package com.bedtime_audio_timer.audiotimer
 
 import android.media.AudioManager
+import java.util.concurrent.TimeUnit
 
 class AudioTimerMath{
     fun currentVolumeToPercentage(am: AudioManager): Int {
@@ -13,6 +14,11 @@ class AudioTimerMath{
 
     fun percentageToVolume(percentage: Int, am: AudioManager): Int {
         return percentage*am.getStreamMaxVolume(AudioManager.STREAM_MUSIC)/100
+    }
+
+    fun findEqualIntervalsInMilliseconds(numMinutes: Int, numIntervals: Int): Long {
+        val numMilliseconds = TimeUnit.MINUTES.toMillis(numMinutes.toLong())
+        return numMilliseconds/numIntervals.toLong()
     }
 
 }
