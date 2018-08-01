cbbfedc82a922da4665b857896b3da80703ac628
Yulia Shidlovskaya <sjullieb@gmail.com>
2018-08-01 00:14:40 -0700

    1. When the layout is destroyed (e.g. rotating device) activity instance is saved and restored. 2. Hold down volume and time increase/decrease buttons continue increasing/decreasing numbers. 3. 2 variable were added that hold volume and minutes increments. 4. The volume's icon is changing depending of chosen volume. 4 levels: mute, minimum, medium, maximum. 5. Pause.png was added to drawable to replace the start button when timer is on.

--- app/src/main/java/com/bedtime_audio_timer/audiotimer/MainActivity.kt
+++ app/src/main/java/com/bedtime_audio_timer/audiotimer/MainActivity.kt
@@ -6,7 +6,12 @@ import android.icu.util.UniversalTimeScale.toLong
 import android.media.AudioManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.service.autofill.Validators.and
+import android.view.MotionEvent
 import android.view.View
+import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.TextView
 import com.bedtime_audio_timer.audiotimer.R
 //import com.bedtime_audio_timer.audiotimer.R.id.halveVol
@@ -15,66 +20,180 @@ import java.util.*
 import java.util.concurrent.TimeUnit
 import kotlin.concurrent.schedule
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.view.*
 
 class MainActivity : AppCompatActivity() {
 
-  /*  var targetVol=0
-    var oldVol=0    //stored in case needed for restoring volume
+    var m_volume: Int = 0 // Target volume
+    var m_minutes: Int = 0 // Target minutes
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val audioManager : AudioManager = getSystemService(AUDIO_SERVICE) as AudioManager
+    // timerObjectDelay and timerObjectPeriod for Timer object when UP/DOWN buttons are hold that define its schedule
+    public var timerObjectDelay = 150
+    public var timerObjectPeriod = 150
+    public var volumeIncrement = 1 // Volume increment on UP/DOWN button
+    public var minutesIncrement = 1 // Minutes increment on UP/DOWN button
 
-        halveVol.setOnClickListener {
-            oldVol = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC)
-            targetVol = oldVol/2
-            audioManager.setVolume(targetVol)
-            mainTimer(5 , 30, audioManager) //temporary test parameters to allow for rapid assessment of timer
+
+    private var timer: Timer? = null // this object is used to increases/decreases volume/minutes when a button is hold
+    private lateinit var timerTask: TimerTask
+    private val handler = Handler()
+
+    // enum that defines what button is hold so the TimerRunnable object could increases/decreases volume/minutes
+    enum class ButtonAction {
+        VOLUME_UP, VOLUME_DOWN, TIMER_UP, TIMER_DOWN
+    }
+
+    // class for to update view from the timer thread when a button is hold
+    internal inner class TimerRunnable : Runnable {
+
+        var buttonAction: ButtonAction = ButtonAction.VOLUME_UP
+
+        override fun run() {
+            if (buttonAction == ButtonAction.VOLUME_UP)
+                increaseVolume(null)
+            else if (buttonAction == ButtonAction.VOLUME_DOWN)
+                decreaseVolume(null)
+            else if (buttonAction == ButtonAction.TIMER_UP)
+                increaseTimer(null)
+            else if (buttonAction == ButtonAction.TIMER_DOWN)
+                decreaseTimer(null)
+
+            if (buttonAction == ButtonAction.VOLUME_UP || buttonAction == ButtonAction.VOLUME_DOWN)
+                updateVolume()
+            else
+                updateTimer()
         }
 
     }
-*/
+
+    // class for listeners when a button is hold
+    internal inner class TimerOnTouchListener : View.OnTouchListener {
+
+        var buttonAction: ButtonAction = ButtonAction.VOLUME_UP
+
+        override fun onTouch(view: View, motionEvent: MotionEvent): Boolean {
+            if (motionEvent.action == MotionEvent.ACTION_UP) {
+                stopTimerTask()
+            } else if (motionEvent.action == MotionEvent.ACTION_DOWN) {
+                startTimer(buttonAction)
+            }
+            return false
+        }
+    }
+
     private fun AudioManager.setVolume(volumeIndex: Int){
         this.setStreamVolume(
                 AudioManager.STREAM_MUSIC, volumeIndex, AudioManager.FLAG_SHOW_UI
         )
     } 
 
-    var m_volume: Int = 0
-    var m_minutes: Int = 0
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        m_volume = 50
-        m_minutes = 30
+
+        if (savedInstanceState != null) {
+            m_volume = savedInstanceState.getInt("reply_volume")
+            m_minutes = savedInstanceState.getInt("reply_minutes")
+        }
+        else {
+            // Initail values
+            m_volume = 50
+            m_minutes = 30
+        }
 
         updateVolume()
         updateTimer()
+
+        val viewVolumeUp = findViewById<View>(R.id.imgBtnVolumeUp) as View
+        val timerOnTouchListenerVolumeUp = TimerOnTouchListener()
+        timerOnTouchListenerVolumeUp.buttonAction = ButtonAction.VOLUME_UP
+        viewVolumeUp.setOnTouchListener(timerOnTouchListenerVolumeUp)
+
+        val viewVolumeDown = findViewById<View>(R.id.imgBtnVolumeDown) as View
+        val timerOnTouchListenerVolumeDown = TimerOnTouchListener()
+        timerOnTouchListenerVolumeDown.buttonAction = ButtonAction.VOLUME_DOWN
+        viewVolumeDown.setOnTouchListener(timerOnTouchListenerVolumeDown)
+
+        val viewTimerUp = findViewById<View>(R.id.imgBtnTimerUp) as View
+        val timerOnTouchListenerTimerUp = TimerOnTouchListener()
+        timerOnTouchListenerTimerUp.buttonAction = ButtonAction.TIMER_UP
+        viewTimerUp.setOnTouchListener(timerOnTouchListenerTimerUp)
+
+        val viewTimerDown = findViewById<View>(R.id.imgBtnTimerDown) as View
+        val timerOnTouchListenerTimerDown = TimerOnTouchListener()
+        timerOnTouchListenerTimerDown.buttonAction = ButtonAction.TIMER_DOWN
+        viewTimerDown.setOnTouchListener(timerOnTouchListenerTimerDown)
+
     }
 
-    fun increaseVolume(view: View) {
+    public override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        outState.putInt("reply_volume", m_volume)
+        outState.putInt("reply_minutes", m_minutes)
+    }
+
+
+    fun startTimer(btnAct: ButtonAction) {
+        timer = Timer()
+
+        initializeTimerTask(btnAct)
+
+        timer?.schedule(timerTask, timerObjectDelay.toLong(), timerObjectPeriod.toLong())
+
+    }
+
+    fun stopTimerTask() {
+        timer?.cancel()
+        timer = null
+    }
+
+
+    fun initializeTimerTask(btnAction: ButtonAction) {
+        timerTask = object : TimerTask() {
+            internal var buttonAction = btnAction
+            override fun run() {
+                val buttonAction = btnAction
+                val timerRunnable = TimerRunnable()
+                timerRunnable.buttonAction = btnAction
+                handler.post(timerRunnable)
+            }
+        }
+    }
+
+    fun increaseVolume(view: View?){
         if (m_volume < 100)
-            m_volume += 5
-        //updateValue(view);
+            m_volume += volumeIncrement
         updateVolume()
     }
 
-    fun decreaseVolume(view: View) {
+    fun decreaseVolume(view: View?) {
         if (m_volume > 0)
-            m_volume -= 5
-        //updateValue(view);
+            m_volume -= volumeIncrement
         updateVolume()
     }
 
     fun updateVolume() {
         val showVolumeTextView = findViewById(R.id.textVolume) as TextView
         showVolumeTextView.text = (String.format("%3d", m_volume) + "%")
+
+        val imgVolume = findViewById(R.id.imgVolume) as ImageView
+
+        when (m_volume){
+            0 -> imgVolume.setImageResource(R.drawable.mute)
+            in 1..25 -> imgVolume.setImageResource(R.drawable.volume_min)
+            in 26..50 -> imgVolume.setImageResource(R.drawable.volume_med)
+            else -> imgVolume.setImageResource(R.drawable.volume_max)
+        }
     }
+     /*   if (m_volume == 0) {
+            imgVolume.setImageResource(R.drawable.mute)
+        } elseif (m_volume > 0) && (m_volume <=25){
+            imgVolume.setImageResource(R.drawable.volume)
+
+    }*/
 
     fun updateTimer() {
-        //Get the ID of the button that was clicked
         val hours: Int
         val minutes: Int
 
@@ -86,18 +205,16 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    fun increaseTimer(view: View) {
+    fun increaseTimer(view: View?) {
         if (m_minutes < 100)
-            m_minutes += 5
+            m_minutes += minutesIncrement
         updateTimer()
-        //updateValue(view);
     }
 
-    fun decreaseTimer(view: View) {
+    fun decreaseTimer(view: View?) {
         if (m_minutes > 0)
-            m_minutes -= 5
+            m_minutes -= minutesIncrement
         updateTimer()
-        //updateValue(view);
     }
 
     fun startTimer(view: View)  {
@@ -120,4 +237,6 @@ class MainActivity : AppCompatActivity() {
         val numMilliseconds = TimeUnit.MINUTES.toMillis(numMinutes.toLong())
         return numMilliseconds/numIntervals.toLong()
     }
+
+
 }
