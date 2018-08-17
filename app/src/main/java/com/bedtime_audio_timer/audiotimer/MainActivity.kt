99128022183a7d197f89b0b01fa8c3cae99568a4
Willa Danielle Rawlinson <willa.rawlinson@gmail.com>
2018-08-16 17:37:20 -0700

    Moving global variables to a globals file

--- app/src/main/java/com/bedtime_audio_timer/audiotimer/MainActivity.kt
+++ app/src/main/java/com/bedtime_audio_timer/audiotimer/MainActivity.kt
@@ -18,16 +18,6 @@ import kotlinx.android.synthetic.main.activity_main.view.*
 
 class MainActivity : AppCompatActivity() {
 
-    var m_volume: Int = 0 // Target volume
-    var m_minutes: Int = 0 // Target minutes
-    var timerIsRunning: Boolean = false
-    // timerObjectDelay and timerObjectPeriod for Timer object when UP/DOWN buttons are hold that define its schedule
-    public var timerObjectDelay = 150
-    public var timerObjectPeriod = 150
-    public var volumeIncrement = 1 // Volume increment on UP/DOWN button
-    public var minutesIncrement = 1 // Minutes increment on UP/DOWN button
-
-
     private var timer: Timer? = null // this object is used to increases/decreases volume/minutes when a button is hold
     private lateinit var timerTask: TimerTask
     private val handler = Handler()
---
+++ app/src/main/java/com/bedtime_audio_timer/audiotimer/globals.kt
@@ -0,0 +1,10 @@
+package com.bedtime_audio_timer.audiotimer
+
+public var m_volume: Int = 0 // Target volume
+public var m_minutes: Int = 0 // Target minutes
+public var timerIsRunning: Boolean = false
+// timerObjectDelay and timerObjectPeriod for Timer object when UP/DOWN buttons are hold that define its schedule
+public var timerObjectDelay = 150
+public var timerObjectPeriod = 150
+public var volumeIncrement = 1 // Volume increment on UP/DOWN button
+public var minutesIncrement = 1 // Minutes increment on UP/DOWN button
