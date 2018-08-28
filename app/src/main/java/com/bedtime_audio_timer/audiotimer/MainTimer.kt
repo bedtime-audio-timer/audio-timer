9ed1e747ca344b3ecdeee216e2e866f2a68dbf3f
Yulia Shidlovskaya <sjullieb@gmail.com>
2018-08-27 21:49:48 -0700

    1. AudioTimerApplication.kt contains a class inherited from Application with variable mTimer : MainTimer. The object of the class is created the very first (manifest changed) and exists throughout the app's life. So does mTimer. 2. MainTimer.kt - added fun isRunning - added calls onVolumeChange, onTimerFinished 3. MainActivity.kt Implemented callbacks onVolumeChange, onTimerFinished.

--- app/src/main/java/com/bedtime_audio_timer/audiotimer/MainTimer.kt
+++ app/src/main/java/com/bedtime_audio_timer/audiotimer/MainTimer.kt
@@ -1,39 +1,62 @@
 package com.bedtime_audio_timer.audiotimer
 
 import android.media.AudioManager
+import com.bedtime_audio_timer.audiotimer.R.drawable.timer
 import java.util.*
+import java.util.function.ToLongFunction
 import kotlin.concurrent.schedule
 
 class MainTimer {
 
     private var timerIsRunning: Boolean = false
 
-    var timer= Timer("interval timer", false) //refers to the main timer for the application
+    public interface TimerCallback {
+        fun onTimerFinished()
+        fun onVolumeChange()
+    }
+    var timer: Timer? = null //refers to the main timer for the application
 
-    fun getTimerIsRunning(): Boolean {
-        return timerIsRunning
+
+    fun isRunning(): Boolean {
+        return (timer != null);
     }
 
-    fun setTimerIsRunning(isRunning: Boolean) {
-        timerIsRunning=isRunning
-    }
-
-    fun startMainTimer(timerParams: TimerParameters, am: AudioManager){
+    fun startMainTimer(timerParams: TimerParameters, am: AudioManager, cb: TimerCallback){
         val numIntervals: Int = atMath.findNumIntervals(am, timerParams)
         val numMinutes=timerParams.getMinutes()
-        val intervalLength = atMath.findEqualIntervalsInMilliseconds(numMinutes, numIntervals)
+        timer = Timer("interval timer", false)
+
+        var intervalLength = atMath.findEqualIntervalsInMilliseconds(numMinutes, numIntervals)
         val startVolume = am.getStreamVolume(AudioManager.STREAM_MUSIC)
         var nextVolume = startVolume - 1
+        var interval = 1
+
+/*
         for (interval in 1..numIntervals){
             timer.schedule(intervalLength*interval) {
                 am.setVolume(nextVolume)
                 nextVolume -= 1
             }
         }
+*/
+        timer?.schedule(object : TimerTask() {
+                override fun run() {
+                    am.setVolume(nextVolume)
+                    cb.onVolumeChange();
+                    nextVolume -= 1
+                    interval += 1
+                    if (interval > numIntervals) {
+                        cancelMainTimer()
+                        cb.onTimerFinished()
+                    }
+                }
+        },  intervalLength, intervalLength)
+
     }
 
     fun cancelMainTimer(){
         timer?.cancel() //the ? is the safe call operator in Kotlin
-        timer = Timer("interval timer", false)
+        timer = null
     }
+
 }
