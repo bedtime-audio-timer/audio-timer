8f5b1739543841e28df36f8c8d45aece564963de
Kate Kourbatova <ekateko@uw.edu>
2018-08-28 17:20:23 -0700

    Declare constants in globals.kt since their values should not change

--- app/src/main/java/com/bedtime_audio_timer/audiotimer/globals.kt
+++ app/src/main/java/com/bedtime_audio_timer/audiotimer/globals.kt
@@ -2,7 +2,7 @@ package com.bedtime_audio_timer.audiotimer
 
 val atMath = AudioTimerMath()
 // timerObjectDelay and timerObjectPeriod for Timer object when UP/DOWN buttons are hold that define its schedule
-public var timerObjectDelay = 150
-public var timerObjectPeriod = 150
-public var volumeIncrement = 1 // Volume increment on UP/DOWN button
-public var minutesIncrement = 1 // Minutes increment on UP/DOWN button
+const val timerObjectDelay = 150
+const val timerObjectPeriod = 150
+const val volumeIncrement = 1 // Volume increment on UP/DOWN button
+const val minutesIncrement = 1 // Minutes increment on UP/DOWN button
